24ca8d7f6713956f1442d8b4efc3c1f203499fc5
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-22 17:19:06 -0500

    feat: create default config and auth interceptor for axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,3 +1,32 @@
-import Axios, { AxiosRequestConfig } from 'axios';
+import Axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
 
 import { API_URL } from '@/config/env-vars';
+import storage from '@/utils/storage';
+
+function authRequestInterceptor(config: AxiosRequestConfig) {
+	const token = storage.getToken();
+
+	if (config.headers) {
+		config.headers.Accept = 'application/json';
+	}
+	if (token && config.headers) {
+		config.headers.authorization = `${token}`;
+	}
+
+	return config;
+}
+
+export const axios = Axios.create({
+	baseURL: API_URL,
+});
+
+axios.interceptors.request.use(authRequestInterceptor);
+axios.interceptors.response.use(
+	(response) => {
+		return response.data;
+	},
+	(error) => {
+		const message = error.response?.data?.message || error.message;
+		return Promise.reject(error);
+	}
+);
