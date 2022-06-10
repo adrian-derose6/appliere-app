57fe6243fffd71b12e9e9e7465a837635e807a66
Adrian DeRose <adrian.derose6@gmail.com>
2022-06-10 16:45:02 -0500

    feat(contacts): create useUpdateContact mutation

---
+++ src/features/contacts/api/updateContact.ts
@@ -0,0 +1,43 @@
+import { useMutation } from 'react-query';
+
+import { authFetch } from '@/lib/axios';
+import { MutationConfig, queryClient } from '@/lib/react-query';
+
+import { Contact, ContactMethod } from '@/features/contacts/types';
+
+export type UpdateContactDTO = {
+	data: {
+		firstName?: string;
+		lastName?: string;
+		jobTitle?: string;
+		companies?: string[];
+		location?: string;
+		emails?: ContactMethod[];
+		phones?: ContactMethod[];
+		jobs?: string[];
+		boardId?: string;
+	};
+	contactId: string;
+};
+
+export const updateContact = async ({
+	data,
+	contactId,
+}: UpdateContactDTO): Promise<Contact> => {
+	const res = await authFetch.patch(`/contacts/${contactId}`, data);
+	return res.data.updatedJob;
+};
+
+type UseUpdateContactOptions = {
+	config?: MutationConfig<typeof updateContact>;
+};
+
+export const useUpdateContact = ({ config }: UseUpdateContactOptions = {}) => {
+	return useMutation({
+		onSettled: (data, err, variables) => {
+			queryClient.invalidateQueries(['contacts', variables.contactId]);
+		},
+		...config,
+		mutationFn: updateContact,
+	});
+};
